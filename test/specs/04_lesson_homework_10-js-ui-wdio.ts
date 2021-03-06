86dc97796e61c49537a389e475673f1d93060ed0
Pavlo Gradobik <gradobik@gmailcom>
2021-03-06 22:59:49 +0200

    fix before hook and logIn function

--- test/specs/04_lesson_homework_10-js-ui-wdio.ts
+++ test/specs/04_lesson_homework_10-js-ui-wdio.ts
@@ -1,81 +1,80 @@
-
 // Use http://93.126.97.71:10082/mp3-players to simplify these tests. Mp3 players does not have custom params on details page.
 
 // bonus points:
 // - use preconditions
 // - use dataprovider
-describe('Items', function () {
-    // You must be logged in to use wishlist
+describe("Items", function () {
+  // You must be logged in to use wishlist
+
+  const emailAdress = `${Math.random().toString(36).substring(3)}@mail.mail`;
+  const password = Math.random().toString(36).substring(3);
+  
+  
+  function logIn(mail: string, password: string) {
     
-    const emailAdress = `${Math.random().toString(36).substring(3)}@mail.mail`;
-    const password = Math.random().toString(36).substring(3)
-    
+    const emailInput = $("#input-email");
+    const passwordInput = $("#input-password");
+    const loginButton = $('[value="Login"]');
+
+    emailInput.setValue(mail);
+    passwordInput.setValue(password);
+    loginButton.click();
+  }
+
+  const productsToWishList: string[] = ['iPod Classic','iPod Nano', 'iPod Shuffle', 'iPod Touch'];
+
+  before(function () {
     //#region register/login page locators
-    const firstNameInput = $('#input-firstname');
-    const lastNameInput = $('#input-lastname');
-    const emailInput = $('#input-email');
-    const telephoneInput = $('#input-telephone');
-    const passwordInput = $('#input-password');
-    const confirmPasswordInput = $('#input-confirm');
+    const firstNameInput = $("#input-firstname");
+    const lastNameInput = $("#input-lastname");
+    const emailInput = $("#input-email");
+    const telephoneInput = $("#input-telephone");
+    const passwordInput = $("#input-password");
+    const confirmPasswordInput = $("#input-confirm");
     const privacyPolicyCheckbox = $('[name="agree"]');
     const continueButton = $('[value="Continue"]');
-    const loginButton = $('value="Login"');    
     //#endregion
 
-    function logIn(mail: string, password: string){
-        
-        browser.url('/index.php?route=account/login');
-        emailInput.setValue(mail);
-        passwordInput.setValue(password);
-        loginButton.click();
-    }
+    //#region registration
+    browser.url("/index.php?route=account/register");
+    browser.acceptAlert(); // there is a browser alert pop-up, so this needed.
+    firstNameInput.setValue(Math.random().toString(36).substring(3));
+    lastNameInput.setValue(Math.random().toString(36).substring(3));
+    emailInput.setValue(emailAdress);
+    telephoneInput.setValue(`+${Math.random().toString().slice(2, 11)}`);
+    passwordInput.setValue(password);
+    confirmPasswordInput.setValue(password);
+    privacyPolicyCheckbox.click();
+    continueButton.click();
+    //#endregion
+  });
 
-    const productsToWishList : string[] = ['', '', ''];
+  beforeEach(function () {
+    browser.reloadSession();
+  });
 
-    before(function() {
+  afterEach(function () {
+    browser.deleteAllCookies();
+  });
 
+  productsToWishList.map((product) => {
 
-        //#region registration
-        browser.url('/index.php?route=account/register');
-        browser.acceptAlert(); // there is a browser alert pop-up, so this needed.
-        firstNameInput.setValue(Math.random().toString(36).substring(3));
-        lastNameInput.setValue(Math.random().toString(36).substring(3));
-        emailInput.setValue(emailAdress);
-        telephoneInput.setValue(`+${Math.random().toString().slice(2, 11)}`);
-        passwordInput.setValue(password);
-        confirmPasswordInput.setValue(password);
-        privacyPolicyCheckbox.click();
-        continueButton.click();
-        browser.pause(500);
-        continueButton.click();
-        //#endregion
-    });
+      it("can be added to wishlist", function () {
+        browser.url("/index.php?route=account/login");
+        logIn(emailAdress, password);
+        browser.url("/mp3-players");
     
-    beforeEach(function() {
-        browser.reloadSession();
-    });
+        $(`a=${product}`).click();
+        browser.pause(2000);
+      });
 
-    afterEach(function() {
-        browser.deleteAllCookies();
-    });
-    
-    it.skip('can be added to wishlist', function () {
- 
-    })
- 
-    it.skip('can be selected for comparison by registered user', function () {
- 
-    })
- 
-    it.skip('can be selected for comparison by guest', function () {
- 
-    })
- 
-    it.skip('can be added to cart by guest', function () {
- 
-    })
- 
-    it.skip('can be added to cart by registered user', function () {
- 
-    })
- })
+  });
+
+  it.skip("can be selected for comparison by registered user", function () {});
+
+  it.skip("can be selected for comparison by guest", function () {});
+
+  it.skip("can be added to cart by guest", function () {});
+
+  it.skip("can be added to cart by registered user", function () {});
+});
