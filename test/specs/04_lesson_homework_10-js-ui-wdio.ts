1d17910d06703f04c713151841c1ac773541e772
Pavlo Gradobik <gradobik@gmailcom>
2021-03-07 18:56:07 +0200

    pg_homework_4
    - try to leave the order of Its as is
    
    commit 96513f58e3d8ca3bc5aae0e8446b73f4ff3c6ba6
    Author: Pavlo Gradobik <gradobik@gmailcom>
    Date:   Sun Mar 7 18:53:19 2021 +0200
    
        add prod add to cart by registered user tests
    
    commit 52432a6581d3294176d57ff803a05eb21c6556e8
    Author: Pavlo Gradobik <gradobik@gmailcom>
    Date:   Sun Mar 7 18:28:04 2021 +0200
    
        add product to cart by guest tests
    
    commit 664f77955cf72adf4b5cc9182e63972c1008b903
    Author: Pavlo Gradobik <gradobik@gmailcom>
    Date:   Sun Mar 7 15:03:07 2021 +0200
    
        add compare by guest tests
    
    commit 766924b781aa841abfbab65ed41968d616d686f8
    Author: Pavlo Gradobik <gradobik@gmailcom>
    Date:   Sun Mar 7 14:17:13 2021 +0200
    
        add compare by registered test
    
    commit e5b2aba2e2ca1e75f389d402e0f6c2bd105a0da9
    Author: Pavlo Gradobik <gradobik@gmailcom>
    Date:   Sun Mar 7 12:45:10 2021 +0200
    
        wishlist test small improvements
    
    commit 0dc5f18d6b9e92c442289f0f8883a928a7575e7c
    Author: Pavlo Gradobik <gradobik@gmailcom>
    Date:   Sun Mar 7 12:40:34 2021 +0200
    
        wishlist tests done
    
    commit d1d2698d6072c0d1f78690a329fe39647ef2bf99
    Author: Pavlo Gradobik <gradobik@gmailcom>
    Date:   Sat Mar 6 23:02:19 2021 +0200
    
        fix the a href locator
    
    commit 86dc97796e61c49537a389e475673f1d93060ed0
    Author: Pavlo Gradobik <gradobik@gmailcom>
    Date:   Sat Mar 6 22:59:49 2021 +0200
    
        fix before hook and logIn function
    
    commit 00f19890cfeaf05b61071064a9b120bbce0ef6e9
    Author: Pavlo Gradobik <gradobik@gmailcom>
    Date:   Sat Mar 6 22:24:41 2021 +0200
    
        add preconditions and postconditions

---
+++ test/specs/04_lesson_homework_10-js-ui-wdio.ts
@@ -0,0 +1,278 @@
+// Use http://93.126.97.71:10082/mp3-players to simplify these tests. Mp3 players does not have custom params on details page.
+
+// bonus points:
+// - use preconditions
+// - use dataprovider
+describe("Items", function () {
+  // You must be logged in to use wishlist
+
+  const emailAdress = `${Math.random().toString(36).substring(3)}@mail.mail`;
+  const password = Math.random().toString(36).substring(3);
+  
+  type ProdList = {
+    name: string;
+    link: string;
+  }
+  
+  function logIn(mail: string, password: string) {
+    
+    const emailInput = $("#input-email");
+    const passwordInput = $("#input-password");
+    const loginButton = $('[value="Login"]');
+
+    emailInput.setValue(mail);
+    passwordInput.setValue(password);
+    loginButton.click();
+  }
+
+  const productsToWishList: Array<ProdList> = [{name: 'iPod Classic', link: 'ipod-classic'}, {name: 'iPod Nano', link: 'ipod-nano'}, {name: 'iPod Shuffle', link: 'ipod-shuffle'},{name: 'iPod Touch', link: 'ipod-touch'}];
+
+  before(function () {
+    //#region register/login page locators
+    const firstNameInput = $("#input-firstname");
+    const lastNameInput = $("#input-lastname");
+    const emailInput = $("#input-email");
+    const telephoneInput = $("#input-telephone");
+    const passwordInput = $("#input-password");
+    const confirmPasswordInput = $("#input-confirm");
+    const privacyPolicyCheckbox = $('[name="agree"]');
+    const continueButton = $('[value="Continue"]');
+    //#endregion
+
+    //#region registration
+    browser.url("/index.php?route=account/register");
+    browser.acceptAlert(); // there is a browser alert pop-up, so this needed.
+    firstNameInput.setValue(Math.random().toString(36).substring(3));
+    lastNameInput.setValue(Math.random().toString(36).substring(3));
+    emailInput.setValue(emailAdress);
+    telephoneInput.setValue(`+${Math.random().toString().slice(2, 11)}`);
+    passwordInput.setValue(password);
+    confirmPasswordInput.setValue(password);
+    privacyPolicyCheckbox.click();
+    continueButton.click();
+    //#endregion
+  });
+
+  afterEach(function() {
+    browser.deleteAllCookies();
+  });
+
+  productsToWishList.map((product) => {
+
+      it("can be added to wishlist", function () {
+
+        //#region locators
+        const addToWishListButton = $('[data-original-title="Add to Wish List"]');
+        const wishListLink = $('#wishlist-total');
+        let selectProduct = $(`h4>a[href*=${product.link}]`);
+        let productInWishListTable = $(`img[title="${product.name}"]`);
+        let wishListMessageLink = $(`a[href*=${product.link}]`);
+        const removeButton = $('[data-original-title="Remove"]');
+        const emptyWishlist = $('#content p');
+        // const myAccountButton = $('[title="My Account"]');
+        // const logoutButton = $('a=Logout');
+        //#endregion
+
+        //#region test
+        browser.url("/index.php?route=account/login");
+        logIn(emailAdress, password);
+              
+        browser.url("/mp3-players");
+        browser.pause(500);
+        
+        selectProduct.click();
+        browser.pause(500);
+        addToWishListButton.click();
+        
+        expect(wishListMessageLink).toBeDisplayed();
+        
+        wishListLink.click();
+        browser.pause(200);
+        
+        expect(productInWishListTable).toBeDisplayed();
+        expect(emptyWishlist).not.toBeDisplayed();
+        
+        removeButton.click();
+        // myAccountButton.click();
+        // logoutButton.click();
+        //#endregion
+   
+      });
+
+  });
+
+  productsToWishList.map((product) => {
+    
+    it("can be selected for comparison by registered user", function () {
+      
+      //#region locarors
+      let addProductRocomparetButton = $(`//div[h4/a[text()="${product.name}"]]/following-sibling::div/button[@data-original-title="Compare this Product"]`);   //the most easy way 
+      let compareProductMessageLink = $(`a[href*=${product.link}]`);
+      const productCompareLink = $('a=product comparison');
+      const productAtCompareTable = $('a > strong');
+      const productImageAtcompareTable = $(`img[title="${product.name}"]`);
+      const removeFromCompareButton = $('a=Remove');
+      //#endregion
+
+      //#region test
+      browser.url('/index.php?route=account/login');
+      logIn(emailAdress, password);
+
+      browser.url('http://93.126.97.71:10082/mp3-players');  
+      browser.pause(1200);
+
+      addProductRocomparetButton.click();
+      browser.pause(400);
+      productCompareLink.click()
+      browser.pause(500);
+
+      expect(productAtCompareTable.getText()).toEqual(product.name);
+      expect(productImageAtcompareTable).toBeDisplayed();
+
+      removeFromCompareButton.click();
+     
+      //#endregion
+
+    });
+  });
+
+  productsToWishList.map((product) => {
+
+    it("can be selected for comparison by guest", function () {
+
+      //#region locarors
+      let compateproductButton = $(
+        `//div[h4/a[text()="${product.name}"]]/following-sibling::div/button[@data-original-title="Compare this Product"]`
+      ); //the most easy way
+      let compareProductMessageLink = $(`a[href*=${product.link}]`);
+      const productCompareLink = $('i ~ a[href*=compare]');
+      const productAtCompareTable = $('a > strong');
+      const productImageAtcompareTable = $(`img[title="${product.name}"]`);
+      const removeFromCompareButton = $('a=Remove');
+      const myAccountButton = $('[title="My Account"]');
+      const loginButton = $('a=Login');
+      //#endregion
+
+      //#region test
+
+      browser.url('http://93.126.97.71:10082/mp3-players');  
+      browser.pause(1000);
+
+      myAccountButton.click();
+      browser.pause(100);
+      expect(loginButton).toBeDisplayed();
+      myAccountButton.click();
+      
+      compateproductButton.click();
+      productCompareLink.click()
+      browser.pause(500);
+      expect(compareProductMessageLink).toBeDisplayed();
+      
+      browser.pause(300);
+
+      expect(productAtCompareTable.getText()).toEqual(product.name);
+      expect(productImageAtcompareTable).toBeDisplayed();
+
+      removeFromCompareButton.click();
+      //#endregion
+    });
+  });
+
+  productsToWishList.map((product) => {
+    
+    it("can be added to cart by guest", function () {
+  
+      //#region locators
+      const addToCartButton = $('#button-cart');
+      let selectProduct = $(`h4>a[href*=${product.link}]`);
+      const shoppingCartDropdown = $('#cart');
+      const shoppingCartLink = $('[title="Shopping Cart"]');
+      const productImageAtCartDropDown = $(`img[title="${product.name}"]`);
+      let productLinkFromCartDropDown = $(`a=${product.name}`);
+      const shoppingCartTitle = $('#content h1');
+      const removeFromCartButton = $('[data-original-title="Remove"]');
+      const myAccountButton = $('[title="My Account"]');
+      const loginButton = $('a=Login');
+      //#endregion
+
+      //#region test
+      browser.url('http://93.126.97.71:10082/mp3-players');  
+      browser.pause(1000);
+      myAccountButton.click();
+      browser.pause(100)
+      expect(loginButton).toBeDisplayed();
+      myAccountButton.click();
+
+      selectProduct.click();
+      browser.pause(500)
+      addToCartButton.click();
+      browser.pause(200);
+
+      shoppingCartDropdown.click();
+      expect(productImageAtCartDropDown).toBeDisplayed();
+      expect(productLinkFromCartDropDown).toBeDisplayed();
+
+      shoppingCartLink.click();
+      browser.pause(1000);
+
+      expect(shoppingCartTitle).toBeDisplayed();
+      expect(shoppingCartTitle).toHaveTextContaining('Shopping Cart');
+      expect(productLinkFromCartDropDown).toExist();
+
+      removeFromCartButton.click();
+      //#endregion
+    });
+
+  });
+ 
+  productsToWishList.map((product) => {
+
+    it("can be added to cart by registered user", function () {
+
+      //#region locators
+      const addToCartButton = $("#button-cart");
+      let selectProduct = $(`h4>a[href*=${product.link}]`);
+      const shoppingCartDropdown = $("#cart");
+      const shoppingCartLink = $('[title="Shopping Cart"]');
+      const productImageAtCartDropDown = $(`img[title="${product.name}"]`);
+      let productLinkFromCartDropDown = $(`a=${product.name}`);
+      const shoppingCartTitle = $("#content h1");
+      const removeFromCartButton = $('[data-original-title="Remove"]');
+      const myAccountButton = $('[title="My Account"]');
+      const logoutButton = $('a=Logout');
+      //#endregion
+
+      //#region test
+      browser.url('/index.php?route=account/login');
+      logIn(emailAdress, password);
+
+      browser.url('http://93.126.97.71:10082/mp3-players');  
+      browser.pause(1200);
+      myAccountButton.click();
+      expect(logoutButton).toBeDisplayed();
+
+      selectProduct.click();
+      browser.pause(500)
+      addToCartButton.click();
+      browser.pause(200);
+
+      shoppingCartDropdown.click();
+      expect(productImageAtCartDropDown).toBeDisplayed();
+      expect(productLinkFromCartDropDown).toBeDisplayed();
+
+      shoppingCartLink.click();
+      browser.pause(1000);
+
+      expect(shoppingCartTitle).toBeDisplayed();
+      expect(shoppingCartTitle).toHaveTextContaining('Shopping Cart');
+      expect(productLinkFromCartDropDown).toExist();
+      expect(productImageAtCartDropDown).toExist();
+
+      removeFromCartButton.click();
+      //#endregion
+    });
+
+  });
+
+
+});
